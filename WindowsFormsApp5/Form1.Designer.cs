f158e9d22018bdf0ec8bd27d54d644630a748b71
İbrahim Alaçakal <ibrahim.alacakal@outlook.com>
2022-10-09 04:33:10 +0300

    Proje dosyası ekle.

---
+++ WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGeri = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(16, 15);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(160, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(13, 51);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(342, 308);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.ListBox1_SelectedIndexChanged);
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(374, 47);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(367, 308);
+            this.listBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(645, 15);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Sil";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 372);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "asd";
+            // 
+            // btnGeri
+            // 
+            this.btnGeri.Location = new System.Drawing.Point(255, 15);
+            this.btnGeri.Margin = new System.Windows.Forms.Padding(4);
+            this.btnGeri.Name = "btnGeri";
+            this.btnGeri.Size = new System.Drawing.Size(100, 28);
+            this.btnGeri.TabIndex = 6;
+            this.btnGeri.Text = "Geri";
+            this.btnGeri.UseVisualStyleBackColor = true;
+            this.btnGeri.Click += new System.EventHandler(this.btnGeri_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 407);
+            this.Controls.Add(this.btnGeri);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dosya Gezgini";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGeri;
+    }
+}
+
---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Load += Form1_Load;
+            comboBox1.SelectedIndexChanged += ComboBox1_SelectedIndexChanged;
+            listBox1.SelectedIndexChanged += ListBox1_SelectedIndexChanged;
+        }
+
+        public void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex > -1)
+            {
+                string seciliSurucu = comboBox1.Items[comboBox1.SelectedIndex].ToString();
+
+                //var dizinler = new DirectoryInfo(seciliSurucu);
+
+                var dizinler = Directory.GetDirectories(seciliSurucu);
+
+                listBox1.Items.Clear();
+
+                foreach (var item in dizinler)
+                {
+                    listBox1.Items.Add(item);
+                }
+
+                DirectoryInfo dosya = new DirectoryInfo(seciliSurucu);
+                FileInfo[] dosyalar = dosya.GetFiles();
+                listBox2.Items.Clear();
+
+                foreach (var item in dosyalar)
+                {
+                    listBox2.Items.Add(item);
+                }
+
+                //listBox1.DataSource = dizinler;
+                //listBox1.DisplayMember = "Name";
+                //listBox1.ValueMember = "Name";
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            List<DriveInfo> suruculer = DriveInfo.GetDrives()
+                .Where(s=>s.IsReady==true).ToList();
+
+            //foreach (var item in suruculer)
+            //{
+            //    comboBox1.Items.Add(item.Name);
+            //}
+
+            comboBox1.DataSource = suruculer;
+            comboBox1.ValueMember = "Name";
+            comboBox1.DisplayMember = "Name";
+
+            //data binding
+        }
+
+        private void btnGeri_Click(object sender, EventArgs e)
+        {
+            string seciliKlasor = label1.Text;
+
+            if (label1.Text != comboBox1.SelectedItem.ToString())
+            {         
+                DirectoryInfo klasor = new DirectoryInfo(seciliKlasor);
+                var parent = klasor.Parent.FullName.ToString();
+
+                DirectoryInfo ustKlasor = new DirectoryInfo(parent);
+                DirectoryInfo[] ustKlasoreGidis = ustKlasor.GetDirectories();
+                listBox1.Items.Clear();
+
+                foreach (var item in ustKlasoreGidis)
+                {
+                    listBox1.Items.Add(item.FullName.ToString());
+                    label1.Text = item.Parent.FullName.ToString();
+                }
+
+                FileInfo[] ustDosyayaGidis = ustKlasor.GetFiles();
+                listBox2.Items.Clear();
+
+                foreach (var item in ustDosyayaGidis)
+                {
+                    listBox2.Items.Add(item.Name.ToString());
+                }
+                //FileInfo ustDosya = new FileInfo(parent);
+                //FileInfo[] usdDosyayaGidis = 
+
+
+            }
+
+            //listbox1.datasource = klasorler;
+            //listbox1.displaymember = "name";
+            //listbox1.valuemember = "fullname";
+
+
+        }
+
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+            if (listBox1.SelectedIndex > -1)
+            {
+                string seciliDizin = listBox1.SelectedItem.ToString();
+                var dizin = new DirectoryInfo(seciliDizin);
+
+                FileInfo[] dosyalar = null;
+
+                label1.Text = listBox1.SelectedItem.ToString();
+
+                try
+                {
+                    dosyalar = dizin.GetFiles();
+                }
+                catch (Exception)
+                {
+                    //MessageBox.Show("Dosyaya Erişim Reddedildi!","Uyari",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+                }
+                if (dosyalar != null)
+                {
+                    listBox2.Items.Clear();
+
+                    foreach (var item in dosyalar)
+                    {
+                        listBox2.Items.Add(item.Name.ToString());
+                    }
+                }
+
+
+                //listBox2.DataSource = dosyalar;
+                //listBox2.DisplayMember = "Name";
+                //listBox2.ValueMember = "FullName";
+
+                string seciliKlasor = listBox1.SelectedItem.ToString();
+                DirectoryInfo klasor = new DirectoryInfo(seciliKlasor);
+                DirectoryInfo[] klasorler = null;
+
+                try
+                {
+                    klasorler = klasor.GetDirectories();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Dosyaya Erişim Reddedildi!", "Uyari", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+
+                if (klasorler != null)
+                {
+                    listBox1.Items.Clear();
+
+                    foreach (var item in klasorler)
+                    {
+                        listBox1.Items.Add(item.FullName.ToString());
+                    }
+
+                    //listbox1.datasource = klasorler;
+                    //listbox1.displaymember = "name";
+                    //listbox1.valuemember = "fullname";
+                }
+            }
+
+        }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    
+
+        //private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        //{
+        //    toolStripStatusLabel1.Text = (listBox2.SelectedItem as FileInfo)
+        //        .CreationTime.Date.ToString();
+
+        //    //File.Delete((listBox2.SelectedItem as FileInfo).FullName);
+
+        //}
+    }
+}
